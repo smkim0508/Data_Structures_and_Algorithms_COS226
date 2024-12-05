1ae47f71f9fb6a9726f456dd04f8e22b70bd566c
Sungmin Kim <sungminkim@dynamic-oit-ip4-wifirestricted03-10-48-56-63.princeton.edu>
2024-12-05 16:18:08 -0500

    creating the clustering algorithm for fraud detection

---
+++ fraud/Clustering.java
@@ -0,0 +1,25 @@
+import edu.princeton.cs.algs4.Point2D;
+
+public class Clustering {
+
+    
+    // run the clustering algorithm and create the clusters
+    public Clustering(Point2D[] locations, int k) {
+
+    }
+
+    // return the cluster of the ith location
+    public int clusterOf(int i) {
+
+    }
+
+    // use the clusters to reduce the dimensions of an input
+    public int[] reduceDimensions(int[] input) {
+
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+
+    }
+}
