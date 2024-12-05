55fad3b81d2e0c0e24e26436146cea787673d7ab
Sungmin Kim <sungminkim@dynamic-oit-ip4-wifirestricted03-10-48-56-63.princeton.edu>
2024-12-05 16:29:21 -0500

    updated clustering

--- fraud/Clustering.java
+++ fraud/Clustering.java
@@ -2,9 +2,21 @@ import edu.princeton.cs.algs4.Point2D;
 
 public class Clustering {
 
-    
+    // helper function to check if argument is null
+    private static <T> void checkArgNull(T arg) {
+        if (arg == null) throw new IllegalArgumentException("argument is null");
+    }
+
     // run the clustering algorithm and create the clusters
     public Clustering(Point2D[] locations, int k) {
+        checkArgNull(locations);
+        int loc_length = locations.length;
+
+        for (int i = 0; i < loc_length; i++) checkArgNull(locations[i]);
+        checkArgNull(k);
+        if (k < 1 || k > loc_length)
+            throw new IllegalArgumentException("k outside of expected range");
+
 
     }
 
