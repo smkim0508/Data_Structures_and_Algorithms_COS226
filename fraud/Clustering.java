6f9c7547b6961eb6c6805917d26f1348b374aa04
Sungmin Kim <sungminkim@dynamic-oit-ip4-wifirestricted03-10-48-56-63.princeton.edu>
2024-12-06 16:34:08 -0500

    implemented clustering algorithm

--- fraud/Clustering.java
+++ fraud/Clustering.java
@@ -1,3 +1,7 @@
+import edu.princeton.cs.algs4.CC;
+import edu.princeton.cs.algs4.Edge;
+import edu.princeton.cs.algs4.EdgeWeightedGraph;
+import edu.princeton.cs.algs4.KruskalMST;
 import edu.princeton.cs.algs4.Point2D;
 
 public class Clustering {
@@ -7,22 +11,54 @@ public class Clustering {
         if (arg == null) throw new IllegalArgumentException("argument is null");
     }
 
+    // private array to track clusters
+    private int[] cluster;
+    // private variable to keep track of the number of locations
+    private int locCount;
+
     // run the clustering algorithm and create the clusters
     public Clustering(Point2D[] locations, int k) {
         checkArgNull(locations);
-        int loc_length = locations.length;
+        locCount = locations.length;
 
-        for (int i = 0; i < loc_length; i++) checkArgNull(locations[i]);
+        for (int i = 0; i < locCount; i++) checkArgNull(locations[i]);
         checkArgNull(k);
-        if (k < 1 || k > loc_length)
+        if (k < 1 || k > locCount)
             throw new IllegalArgumentException("k outside of expected range");
 
+        EdgeWeightedGraph locationGraph = new EdgeWeightedGraph(locCount);
+        EdgeWeightedGraph mstGraph = new EdgeWeightedGraph(locCount);
 
+        // EdgeWeightedGraph between all point pairs
+        for (int i = 0; i < locCount; i++) {
+            for (int j = i + 1; j < locCount; j++) {
+                locationGraph.addEdge(new Edge(i, j, locations[i].distanceTo(locations[j])));
+            }
+        }
+
+        // fill mst
+        KruskalMST mst = new KruskalMST(locationGraph);
+        int edgeCount = 0;
+        for (Edge edge : mst.edges()) {
+            if (edgeCount < locCount - k) {
+                mstGraph.addEdge(edge);
+                edgeCount++;
+            }
+        }
+
+        // finds connected components
+        CC connected = new CC(mstGraph);
+        cluster = new int[locCount];
+        for (int i = 0; i < locCount; i++) {
+            cluster[i] = connected.id(i);
+        }
     }
 
     // return the cluster of the ith location
     public int clusterOf(int i) {
-
+        checkArgNull(i);
+        if (i < 0 || i > locCount - 1) throw new IllegalArgumentException("location out of bounds");
+        return cluster[i];
     }
 
     // use the clusters to reduce the dimensions of an input
